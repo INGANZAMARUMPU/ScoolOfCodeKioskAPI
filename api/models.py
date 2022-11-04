0e11248b5f1c191fe1da33d65e1ab8340758e4b5
INGANZAMARUMPU <jonkur96@gmail.com>
2022-11-04 18:29:15 +0200

    created database

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Produit(models.Model):
+	id = models.AutoField(primary_key=True)
+	nom = models.CharField(max_length=64)
+	prix_vente = models.IntegerField()
+
+class Achat(models.Model):
+	id = models.BigAutoField(primary_key=True)
+	produit = models.ForeignKey(Produit, on_delete=models.PROTECT)
+	quantite = models.IntegerField()
+	prix_total = models.IntegerField()
+	date = models.DateField(auto_now_add=True)
+	user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+class Vente(models.Model):
+	id = models.BigAutoField(primary_key=True)
+	produit = models.ForeignKey(Produit, on_delete=models.PROTECT)
+	quantite = models.IntegerField()
+	prix = models.IntegerField()
+	date = models.DateField(auto_now_add=True)
+	user = models.ForeignKey(User, on_delete=models.PROTECT)
